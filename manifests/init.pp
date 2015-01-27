4f05e1c946d6423e3e01e2df7f40b39f32ccd572
etlweather <etlweather@keylimebox.org>
2015-01-26 19:09:07 -0500

    Initial version

---
+++ manifests/init.pp
@@ -0,0 +1,38 @@
+# Class: odoo
+
+class odoo (
+             $admin_passwd = '',
+             $db_host      = 'localhost',
+             $db_port      = '5432',
+             $db_user      = 'odoo',
+             $db_password  = 'odoo',
+             $addons_path  = '/opt/odoo/addons'
+            ) {
+
+   case $::operatingsystem {
+      /(Ubuntu)/: {
+         $package_name  = 'odoo'
+      }
+      default: {
+         fail ("Module ${module_name} is not supported on ${::operatingsystem}")
+      }
+   }
+
+   include odoo::install
+
+   if $admin_passwd.start_with?('ENC[') {
+      $admin_passwd = decrypt ($admin_passwd)
+   }
+   if $db_password.start_with?('ENC[') {
+      $db_password = decrypt ($db_password)
+   }
+
+   file { 'openerp-server.conf':
+      path    => '/etc/odoo/openerp-server.conf',
+      content => template("${module_name}/openerp-server.conf.erb"),
+      owner   => 'odoo',
+      group   => 'odoo',
+      mode    => '0640',
+   }
+
+}
---
+++ manifests/install.pp
@@ -0,0 +1,123 @@
+# Class: odoo::install
+#
+# Installs all the packages necessary for Odoo on Ubuntu.
+#
+class odoo::install {
+   if !defined(Class['odoo']) {
+      fail ('You must include the odoo base class before using any odoo subclass')
+   }
+
+   ## Dependencies
+   package {'python-dateutil':
+      ensure   => present,
+   }
+   package {'python-feedparser':
+      ensure   => present,
+   }
+   package {'python-gdata':
+      ensure   => present,
+   }
+   package {'python-ldap':
+      ensure   => present,
+   }
+   package {'python-libxslt1':
+      ensure   => present,
+   }
+   package {'python-lxml':
+      ensure   => present,
+   }
+   package {'python-mako':
+      ensure   => present,
+   }
+   package {'python-openid':
+      ensure   => present,
+   }
+   package {'python-psycopg2':
+      ensure   => present,
+   }
+   package {'python-pybabel':
+      ensure   => present,
+   }
+   package {'python-pychart':
+      ensure   => present,
+   }
+   package {'python-pydot':
+      ensure   => present,
+   }
+   package {'python-pyparsing':
+      ensure   => present,
+   }
+   package {'python-reportlab':
+      ensure   => present,
+   }
+   package {'python-simplejson':
+      ensure   => present,
+   }
+   package {'python-tz':
+      ensure   => present,
+   }
+   package {'python-vatnumber':
+      ensure   => present,
+   }
+   package {'python-vobject':
+      ensure   => present,
+   }
+   package {'python-webdav':
+      ensure   => present,
+   }
+   package {'python-werkzeug':
+      ensure   => present,
+   }
+   package {'python-xlwt':
+      ensure   => present,
+   }
+   package {'python-yaml':
+      ensure   => present,
+   }
+   package {'python-zsi':
+      ensure   => present,
+   }
+   package {'postgresql-client':
+      ensure   => present,
+   }
+   package {'python-decorator':
+      ensure   => present,
+   }
+   package {'python-docutils':
+      ensure   => present,
+   }
+   package {'python-imaging':
+      ensure   => present,
+   }
+   package {'python-jinja2':
+      ensure   => present,
+   }
+   package {'python-mock':
+      ensure   => present,
+   }
+   package {'python-passlib':
+      ensure   => present,
+   }
+   package {'python-psutil':
+      ensure   => present,
+   }
+   package {'python-pypdf':
+      ensure   => present,
+   }
+   package {'python-unittest2':
+      ensure   => present,
+   }
+   package {'wkhtmltox':
+      ensure => '0.12.1',
+   }
+   file {'/usr/bin/wkhtmltopdf':
+      ensure   => link,
+      target   => '/usr/local/bin/wkhtmltopdf'
+   }
+
+   ## Odoo itself
+   package {'odoo':
+      ensure   => '8.0.0',
+   }
+
+}
